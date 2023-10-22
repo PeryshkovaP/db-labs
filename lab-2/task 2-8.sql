61d27d94f21e967e0f39f08f0b4514d9618a2077
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-10-22 11:57:22 +0400

    task 2-8.sql

--- lab-2/task 2-8.sql
+++ lab-2/task 2-8.sql
@@ -1,6 +1,6 @@
 /*Выбрать имена объектов с их качественной стоимостью 
 (качественной стоимостью == ‘дешевый’ если monthlymaintenance < 100 и ‘дорогой’ в противном случае*/
-select * from facilities;
-select facility, 'Дешевый' as 'Качественная стоимость' from facilities where monthlymaintenance < 100
-union
-select facility, 'Дорогой' from facilities where monthlymaintenance >= 100;
+USE cd;
+SELECT facility, 'Дешевый' AS 'Качественная стоимость' FROM facilities WHERE monthlymaintenance < 100
+UNION
+SELECT facility, 'Дорогой' FROM facilities WHERE monthlymaintenance >= 100;
