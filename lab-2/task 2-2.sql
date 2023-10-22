dcb3a3c3c6461949f49cbf23a812f1e9fe4f1c1f
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-10-22 11:37:58 +0400

    task 2-2.sql

--- lab-2/task 2-2.sql
+++ lab-2/task 2-2.sql
@@ -1,3 +1,3 @@
 /*Выбрать информацию о наименовании и стоимости его использования для членов клуба (membercost) для всех зарегистрированных в базе объектах (facilities) клуба.*/
-use cd;
-select facility, membercost from facilities
+USE cd;
+SELECT facility, membercost FROM facilities
