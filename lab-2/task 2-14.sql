2f11f196ad0362db485d00f6d67a9784abf69d5b
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-10-22 12:25:14 +0400

    task 2-14.sql

--- lab-2/task 2-14.sql
+++ lab-2/task 2-14.sql
@@ -1,5 +1,6 @@
 /*Выберите самый дорогой и самый дешевый объект.*/
-select max(initialoutlay) from facilities;
-select facility from facilities where initialoutlay = (select max(initialoutlay) from facilities) 
-or initialoutlay = (select min(initialoutlay) from facilities);
+USE cd;
+SELECT facility, 'Самый дорогой' AS 'Качественная стоимость' FROM facilities WHERE monthlymaintenance = (SELECT MAX(monthlymaintenance) FROM facilities)
+UNION 
+SELECT facility, 'Самый дешёвый' FROM facilities WHERE monthlymaintenance = (SELECT MIN(monthlymaintenance) FROM facilities);
 
