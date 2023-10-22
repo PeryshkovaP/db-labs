0d3223efbef74c3c4e0e4a3fa61c90f5b1c0923c
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-10-22 12:23:36 +0400

    task 2-13.sql

--- lab-2/task 2-13.sql
+++ lab-2/task 2-13.sql
@@ -1,3 +1,3 @@
 /*Выберите имя, фамилию и дату вступления в клуб последних из всех вступивших.*/
-select m.surname, m.firstname, b.starttime from members as m join bookings as b on m.memid = b.memid 
-where starttime = (select max(starttime) from bookings);
+USE cd;
+SELECT firstname, surname, joindate FROM members WHERE joindate = (SELECT MAX(joindate) FROM members);
