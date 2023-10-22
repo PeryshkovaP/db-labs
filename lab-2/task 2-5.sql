587b2f4181a851d379ffe4eba54fddc34709e39d
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-10-22 11:47:11 +0400

    task 2-5.sql

--- lab-2/task 2-5.sql
+++ lab-2/task 2-5.sql
@@ -1,5 +1,5 @@
 /* Выбрать объекты, которые платны для членов клуба, но стоимость 
 использование не превышает 1/50 от стоимость месячного обслуживания 
 (monthlymaintenance).*/
-use cd;
-select facility from facilities where guestcost < (monthlymaintenance/50) or membercost < (monthlymaintenance/50) and membercost != 0;
+USE cd;
+SELECT facility FROM facilities WHERE membercost <= (monthlymaintenance/50) AND membercost != 0;
