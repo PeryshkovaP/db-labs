f14690afd409cc6671c1f7b10fb63c6b475f4528
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-10-28 15:09:22 +0400

    task 3-7.sql

--- lab-3/task 3-7.sql
+++ lab-3/task 3-7.sql
@@ -1,7 +1,20 @@
 /*Выберите список всех членов, включая человека, который их рекомендовал (если таковой имеется), 
 без использования каких-либо объединений. Исключите в списке дубликаты, упорядочите лист по ФИО (== имя + фамилия).*/
-USE cd; 
-SELECT DISTINCT concat(m1.surname, ' ', m1.firstname) AS fiomember, concat(m2.surname, ' ', m2.firstname) AS fiorecommend 
-FROM members m1, members m2 
-WHERE m1.recommendedby = m2.memid AND m1.memid != 0
+USE cd;
+SELECT 
+    concat(m1.surname, ' ', m1.firstname) AS fiomember
+FROM 
+    members m1
+WHERE 
+    EXISTS (
+        SELECT 1
+        FROM members AS m2
+        WHERE m2.memid = m1.recommendedby
+    )
+OR 
+    NOT EXISTS (
+        SELECT 1
+        FROM members AS m3
+        WHERE m3.memid = m1.recommendedby
+    )
 ORDER BY fiomember;
