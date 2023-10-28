00953168b4f372ef189d51e238f8931fdb002b59
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-10-28 14:16:17 +0400

    task 3-6.sql

--- lab-3/task 3-6.sql
+++ lab-3/task 3-6.sql
@@ -4,7 +4,7 @@
 Помните, что у гостей разные затраты по сравнению с участниками (указанные затраты указаны за получасовой «интервал»), 
 а идентификатор гостя всегда имеет идентификатор 0.*/
 USE cd;
-SELECT DISTINCT concat(f.facility, ' ', m.firstname) AS FacilityAndName, 
+SELECT DISTINCT CONCAT(f.facility, ' ', m.firstname) AS FacilityAndName, 
 CASE 
 WHEN b.memid = 0 THEN f.guestcost * b.slots
 ELSE f.membercost * b.slots 
@@ -12,6 +12,9 @@ END AS cost
 FROM bookings b
 JOIN facilities f ON b.facid = f.facid
 JOIN members m ON b.memid = m.memid
-WHERE DATE(b.starttime) = '2012-09-14'
-HAVING cost > 30
+WHERE DATE(b.starttime) = '2012-09-14' AND
+(CASE 
+WHEN b.memid = 0 THEN f.guestcost * b.slots
+ELSE f.membercost * b.slots 
+END) > 30
 ORDER BY cost DESC;
