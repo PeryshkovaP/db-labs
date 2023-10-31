4a3b8a936d7bd665747249886390271745ea2de1
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-10-31 18:17:30 +0400

    task 3-6.sql

--- lab-3/task 3-6.sql
+++ lab-3/task 3-6.sql
@@ -12,9 +12,5 @@ END AS cost
 FROM bookings b
 JOIN facilities f ON b.facid = f.facid
 JOIN members m ON b.memid = m.memid
-WHERE DATE(b.starttime) = '2012-09-14' AND
-(CASE 
-WHEN b.memid = 0 THEN f.guestcost * b.slots
-ELSE f.membercost * b.slots 
-END) > 30
+WHERE DATE(b.starttime) = '2012-09-14' AND ((b.memid = 0 AND  f.guestcost * b.slots > 30) OR (b.memid != 0 AND f.membercost * b.slots > 30)) 
 ORDER BY cost DESC;
