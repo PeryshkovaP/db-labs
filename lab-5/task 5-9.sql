b3730bbcf6faa3cbef122df11ae256d403594592
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-11-16 15:38:20 +0400

    task 5-9

--- lab-5/task 5-9.sql
+++ lab-5/task 5-9.sql
@@ -1,5 +1,5 @@
 /*Создайте список объектов с общим доходом менее 1000. Создайте выходную таблицу, состоящую 
 из названия объекта и дохода, отсортированного по доходу. Помните, что для гостей и участников действуют разные цены!*/
 USE cd;
-SELECT facility, SUM(IF(b.memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) AS revenue from facilities f
+SELECT facility, SUM(IF(b.memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) AS revenue FROM facilities f
 LEFT JOIN bookings b ON b.facid = f.facid GROUP BY f.facility HAVING revenue < 1000 ORDER BY revenue;
