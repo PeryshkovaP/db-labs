bece700c452d3448d3f5e6212cb35647c59401bf
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-11-19 17:50:35 +0400

    new task 5-3

--- lab-5/task 5-3.sql
+++ lab-5/task 5-3.sql
@@ -1,4 +1,4 @@
 /*Рассчитайте для каждого члена количество рекомендаций, данных этим членом клуба, искючив членов, которые не давали рекомендаций.*/
 USE cd;
 SELECT CONCAT(m1.surname, ' ', m1.firstname) AS 'ФИО', COUNT(m2.memid) AS 'Количество рекомендаций' FROM members m1
-LEFT JOIN members AS m2 ON m2.recommendedby = m1.memid WHERE m2.memid IS NOT NULL GROUP BY m1.memid;
+JOIN members AS m2 ON m2.recommendedby = m1.memid GROUP BY m1.memid;
