7c904d51e32556c304cc0bc3b7014443aed19e94
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-11-20 14:58:13 +0400

    new task 5-14

--- lab-5/task 5-14.sql
+++ lab-5/task 5-14.sql
@@ -1,4 +1,5 @@
 /*Выведите идентификатор объекта, в котором забронировано наибольшее количество мест (slot). Убедитесь, что в случае ничьей все результаты привязки будут выведены.
 Примечание: используйте функцию ранжирования*/
 USE cd;
-SELECT facid, slots FROM (SELECT facid, slots, RANK() OVER (ORDER BY slots DESC) AS maxrank FROM bookings) AS ranked_bookings WHERE maxrank = 1;
+SELECT DISTINCT facid, slots FROM (SELECT facid, slots, RANK() OVER (ORDER BY slots DESC) as maxrank FROM bookings) ranked_bookings
+WHERE maxrank = 1;
