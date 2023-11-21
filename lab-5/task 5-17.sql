faf59ef9613baddea76f0c5e2770e721dc660fd3
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-11-21 18:20:58 +0400

    new task 5-17

--- lab-5/task 5-17.sql
+++ lab-5/task 5-17.sql
@@ -4,12 +4,13 @@
 USE cd;
 SELECT facility, 
 CASE 
-  WHEN NTILE(3) OVER (ORDER BY revenue) = 1 THEN 'Высокий доход'
-  WHEN NTILE(3) OVER (ORDER BY revenue) = 2 THEN 'Средний доход'
-  WHEN NTILE(3) OVER (ORDER BY revenue) = 3 THEN 'Низкий доход'
+  WHEN incomegroup = 1 THEN 'Высокий доход'
+  WHEN incomegroup = 2 THEN 'Средний доход'
+  WHEN incomegroup = 3 THEN 'Низкий доход'
 END AS incomegroup
 FROM (
-  SELECT f.facility, SUM(IF(b.memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) AS revenue
+  SELECT f.facility, SUM(IF(b.memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) AS revenue,
+  NTILE(3) OVER (ORDER BY SUM(IF(b.memid = 0, f.guestcost * b.slots, f.membercost * b.slots))) AS incomegroup
   FROM facilities f
   INNER JOIN bookings b ON f.facid = b.facid
   GROUP BY f.facility
