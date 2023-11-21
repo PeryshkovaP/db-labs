aadeeb00c8e6fb43cf80cf5898547b6d334f1f8c
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-11-21 22:01:54 +0400

    new task 5-18

--- lab-5/task 5-18.sql
+++ lab-5/task 5-18.sql
@@ -4,9 +4,14 @@
 Примечание: используйте DATE_ADD для генерации серии дат.*/
 USE cd;
 SELECT 	DATE(b.starttime) AS date,
-		SUM(IF(memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) as revenue,
-		AVG(IF(memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) AS moving_average
-FROM bookings b
-LEFT JOIN facilities f ON b.facid = f.facid WHERE DATE(b.starttime) BETWEEN '2012-08-01' AND '2012-08-31'
-GROUP BY DATE(b.starttime) HAVING DATE_ADD(date, INTERVAL 15 DAY) <= '2012-08-31' 
+		SUM(IF(memid = 0, f.guestcost * b.slots, f.membercost * b.slots)) AS revenue,
+		(SELECT AVG(IF(memid = 0, f.guestcost * b2.slots, f.membercost * b2.slots))
+		FROM bookings b2
+		JOIN facilities f ON b2.facid = f.facid
+		WHERE DATE(b2.starttime) BETWEEN DATE_SUB(b.starttime, INTERVAL 15 DAY) AND DATE_SUB(b.starttime, INTERVAL 1 DAY)
+		) AS moving_average FROM bookings b
+JOIN facilities f ON b.facid = f.facid
+WHERE DATE(b.starttime) BETWEEN '2012-08-01' AND '2012-08-31'
+GROUP BY DATE(b.starttime), moving_average
+HAVING DATE_ADD(date, INTERVAL 15 DAY) <= '2012-08-31'
 ORDER BY DATE(b.starttime);
