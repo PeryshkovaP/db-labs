7573e9e54a50a54fb9a20818e8a28c4e9efc48e4
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-12-10 16:56:04 +0400

    new task 7-2

--- lab-7/task 7-2.sql
+++ lab-7/task 7-2.sql
@@ -1,64 +1,63 @@
 /*Создайте таблицу payments со структурой (payid INT PK, FK on booking.bookid; payment  DECIMAL.)*/
 USE cd;
 CREATE TABLE payments (
-  payid INT PRIMARY KEY,
+  payid INT PRIMARY KEY AUTO_INCREMENT,
   bookid INT,
   payment DECIMAL,
   FOREIGN KEY (bookid) REFERENCES cd.bookings(bookid)
 );
 /*Добавьте в таблицу bookings поле payed, смысл которого оплачена или не оплачена аренда.*/
-ALTER TABLE bookings ADD COLUMN payed BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE bookings ADD COLUMN payed TINYINT DEFAULT 0;
 /*Создайте триггеры, которые: 1) Запрещают удаление записей, если они уже оплачены;*/
 DELIMITER $$
 
-DROP TRIGGER IF EXISTS prevent_payment_deletion;
+DROP TRIGGER IF EXISTS prevent_payment_deletion $$
 CREATE TRIGGER prevent_payment_deletion 
 BEFORE DELETE ON bookings 
 FOR EACH ROW 
 BEGIN
-  IF OLD.payed = TRUE THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Can not delete a paid booking';
+  IF (OLD.payed = 1) THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Can not delete a paid booking!';
   END IF;
 END$$
 
 DELIMITER ;
+
 /*2) После отметки оплаты, заносят в таблицу payments запись с соответствующим значением PK 
 и суммой оплаты, для вычисления которой используется функция созданная в Task-7-1.*/
-DELIMITER $$
-
-DROP TRIGGER IF EXISTS record_payment;
-CREATE TRIGGER record_payment AFTER UPDATE ON bookings 
-FOR EACH ROW 
-BEGIN
-  IF NEW.payed = TRUE THEN
-    INSERT INTO payments (payid, bookid, payment) VALUES (NEW.bookid, NEW.payment);
-  END IF;
-END$$
-
-DELIMITER ;
 /*3) При отмене оплаты - удаляет соответствующую запись в таблице payments.*/
 DELIMITER $$
 
-DROP TRIGGER IF EXISTS delete_payment;
-CREATE TRIGGER delete_payment 
+DROP TRIGGER IF EXISTS insert_payment_record $$
+CREATE TRIGGER insert_payment_record
 AFTER UPDATE ON bookings
 FOR EACH ROW
 BEGIN
-  IF NEW.payed = FALSE THEN
-    DELETE FROM payments WHERE bookid = NEW.bookid;
-  END IF;
-END$$
+  IF NEW.payed != OLD.payed THEN
+  CASE
+		WHEN NEW.payed = 1 THEN
+			INSERT INTO payments (bookid, payment) VALUES (NEW.bookid, CalculateRentalCost(NEW.memid, NEW.facid, NEW.slots));
+		WHEN NEW.payed = 0 THEN
+        DELETE FROM payments pay 
+        WHERE pay.bookid =  NEW.bookid;
+	END CASE;
+   END IF;
+END $$
 
 DELIMITER ;
 /*Напишите скрипт, который отмечает, что все аренды июля 2012 года оплачены.*/
+
 UPDATE bookings 
-SET payed = TRUE 
+SET payed = 1
 WHERE YEAR(starttime) = 2012 AND MONTH(starttime) = 7;
+
 /*Посчитайте (написав соответствующий скрипт) оплату на июль 2012 года двумя способами: 1) используя данные таблицы payments*/
+
 SELECT SUM(payment) as costJuly1
-FROM payments 
-WHERE bookid IN (SELECT bookid FROM bookings WHERE YEAR(starttime) = 2012 AND MONTH(starttime) = 7);
+FROM payments;
+
 /*2) используя только функцию из Task-7-1 и данные таблицы bookings.*/
+
 SELECT SUM(CalculateRentalCost(memid, facid, slots)) as costJuly2
 FROM bookings 
 WHERE YEAR(starttime) = 2012 AND MONTH(starttime) = 7;
