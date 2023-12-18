ee30d8a25059e37ef93df8b6ee315df23de77df4
PeryshkovaP <147165843+PeryshkovaP@users.noreply.github.com>
2023-12-18 15:09:34 +0400

    new task 7-1

--- lab-7/task 7-1.sql
+++ lab-7/task 7-1.sql
@@ -1,7 +1,7 @@
 /*Создайте функцию, которая рассчитывает стоимость каждой аренды (для каждой записи таблицы bookings)*/
 USE cd;
 DELIMITER $$
-DROP FUNCTION IF EXISTS CalculateRentalCost;
+DROP FUNCTION IF EXISTS CalculateRentalCost $$
 CREATE FUNCTION CalculateRentalCost(memid INT, facid INT, slots INT)
 RETURNS DECIMAL(10, 2)
 DETERMINISTIC
